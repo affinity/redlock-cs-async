26abdac45e67411492e462855cea7b4035d1e093
Collin Sauve <collinsauve@gmail.com>
2017-01-13 14:22:02 -0500

    unify all AssemblyInfo.cs files

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("redlock-cs")]
+[assembly: AssemblyDescription("Asynchronous distributed lock with Redis and C# (based on http://redis.io/topics/distlock)")]
+[assembly: AssemblyCompany("Nudge Software Inc")]
+[assembly: AssemblyProduct("redlock-cs-async")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
