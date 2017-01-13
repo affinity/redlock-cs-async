ac3074cdb0004876db2f6e524bb43a39581a8001
Collin Sauve <collinsauve@gmail.com>
2017-01-13 14:06:21 -0500

    delete old scripts

--- build.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-	[String]$task = "Build-Project"
-)
-
-Invoke-WebRequest https://github.com/psake/psake/raw/master/psake.psm1 -OutFile psake.psm1
-Import-Module .\psake.psm1
-
-Invoke-Psake $task
